758fc5ab5ffd3eabed9d32cb0bb064538149190a
ccaverotx <ccaverotx@gmail.com>
2025-05-25 14:42:30 -0400

    refactor(flake):correct directory

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.${myUsername} = import ./users/${myUsername}.nix;
+            home-manager.users.${myUsername} = import ./modules/home;
           }
         ];
 
--- modules/home/default.nix
+++ modules/home/default.nix
@@ -3,6 +3,8 @@
   home.homeDirectory = "/home/ccaverotx";
   home.stateVersion = "24.05";
 
+  home.packages = [ pkgs.home-manager ];
+
   imports = [
     ./programs/gui-apps/firefox
     ./programs/gui-apps/vscode
