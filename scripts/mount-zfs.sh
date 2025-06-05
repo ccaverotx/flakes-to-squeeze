1e89c1b26dfe74a6152bd01d445b3d84dca6f7a1
Carlos Cavero <54409608+ccaverotx@users.noreply.github.com>
2025-06-05 16:13:55 +0000

    Update mount-zfs.sh

--- scripts/mount-zfs.sh
+++ scripts/mount-zfs.sh
@@ -5,11 +5,8 @@ POOL="zroot"
 PREFIX="nixos"
 USER="ccaverotx"
 
-echo "[+] Montando / como tmpfs..."
+echo "[+] Masdfasdfasasdfontando / como tmpfs..."
 mount -t tmpfs -o mode=755 tmpfs /mnt
-
-echo "[+] Importando zpool $POOL..."
-#zpool import "$POOL"
 
 echo "[+] Creando puntos de montaje..."
 mkdir -p /mnt/nix
@@ -25,7 +22,7 @@ mount -t zfs "$POOL/$PREFIX/persist" /mnt/persist
 mount -t zfs "$POOL/$PREFIX/persist/etc-nixos" /mnt/etc/nixos
 mount -t zfs "$POOL/$PREFIX/persist/var" /mnt/var
 mount -t zfs "$POOL/$PREFIX/persist/home" /mnt/home
-#mount -t zfs "$POOL/$PREFIX/persist/home/$USER" "/mnt/home/$USER"
+mount -t zfs "$POOL/$PREFIX/persist/home/$USER" "/mnt/home/$USER"
 
 echo "[+] Montando partición EFI en /mnt/boot..."
 mount /dev/nvme0n1p1 /mnt/boot
