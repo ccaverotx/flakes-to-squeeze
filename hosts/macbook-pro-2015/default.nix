fdd41ecc301be5cba00e5e8ea501bd9ae465a4cc
ccaverotx <ccaverotx@gmail.com>
2025-06-16 16:13:53 -0400

    refactor(hosts): added macbook pkgs

--- hosts/macbook-pro-2015/default.nix
+++ hosts/macbook-pro-2015/default.nix
@@ -20,8 +20,57 @@
   hyprlandSystem.enable = true;
   virtualisation.podman.enable = true;
 
+  networking.networkmanager.enable = true;
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  services.tlp.enable = true;
+
+  hardware.sensor.iio.enable = true;
+  hardware.lm-sensors.enable = true;
+
+  services.mbpfan = {
+    enable = true;
+    settings.general = {
+      verbose = true;
+      log_level = 2;
+      daemon = true;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     postgresql
+
+    # Red
+    networkmanager
+    networkmanagerapplet
+
+    # Gestión térmica
+    mbpfan
+
+    # Brillo y retroiluminación
+    brightnessctl
+    acpilight
+
+    # Sensores y energía
+    tlp
+    acpi
+    lm_sensors
+
+    # Audio
+    pulseaudio
+
+    # Bluetooth
+    bluez
+    bluez-tools
+    blueman
+
+    # Utilidades generales
+    unzip
+    htop
+    pciutils
+    usbutils
   ];
 
   # Necesario para que impermanence funcione correctamente
