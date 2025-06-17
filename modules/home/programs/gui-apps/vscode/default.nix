be437c54514e4da6a56b79f785919c6b80e1e24c
ccaverotx <ccaverotx@gmail.com>
2025-06-16 23:30:59 -0400

    feat(vscode): macbook tweaks

--- modules/home/programs/gui-apps/vscode/default.nix
+++ modules/home/programs/gui-apps/vscode/default.nix
@@ -1,3 +1,22 @@
-{ pkgs, ... }: {
-  home.packages = [ pkgs.vscode ];
+{ config, pkgs, lib, ... }:
+
+let
+  hostType = config.hostType;
+
+  vscodePackage = if hostType == "macbook-pro-2015" then
+    (pkgs.writeShellScriptBin "code" ''
+      export NIXOS_OZONE_WL=1
+      export ELECTRON_OZONE_PLATFORM_HINT=auto
+      exec ${pkgs.vscode}/bin/code --ozone-platform=wayland
+    '')
+  else
+    pkgs.vscode;
+
+in {
+  home.packages = [ vscodePackage ];
+
+  home.sessionVariables = lib.mkIf (hostType == "macbook-pro-2015") {
+    NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+  };
 }
