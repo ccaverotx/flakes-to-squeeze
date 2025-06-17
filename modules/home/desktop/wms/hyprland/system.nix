ec70c702b0d987ba48a33e42694cbb31621f89e9
ccaverotx <ccaverotx@gmail.com>
2025-06-17 12:07:18 -0400

    feat(vscode): macbook tweaks

--- modules/home/desktop/wms/hyprland/system.nix
+++ modules/home/desktop/wms/hyprland/system.nix
@@ -1,6 +1,14 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  hostname = config.networking.hostName or "unknown";
+  hostType =
+    if hostname == "macbook-pro-2015" then "laptop"
+    else if hostname == "desktop" then "desktop"
+    else "unknown";
+
+  isHiDPI = hostType == "laptop"; # o ajusta según lo necesites
+in {
   options.hyprlandSystem.enable = lib.mkEnableOption "Enable Hyprland system-level configuration";
 
   config = lib.mkIf config.hyprlandSystem.enable {
@@ -16,13 +24,16 @@
       };
     };
 
-    environment.sessionVariables = {
-      GDK_SCALE = "2";
-      XCURSOR_THEME = "Nordzy-cursors-white";
-      XCURSOR_SIZE = "30";
-      ELECTRON_ENABLE_HIGH_DPI_SCALING = "1";
-      ELECTRON_FORCE_DEVICE_SCALE_FACTOR = "2";
-    };
+    environment.sessionVariables =
+      {
+        XCURSOR_THEME = "Nordzy-cursors-white";
+        XCURSOR_SIZE = "30";
+        ELECTRON_ENABLE_HIGH_DPI_SCALING = "1";
+      }
+      // lib.optionalAttrs isHiDPI {
+        GDK_SCALE = "2";
+        ELECTRON_FORCE_DEVICE_SCALE_FACTOR = "2";
+      };
 
     environment.systemPackages = with pkgs; [
       greetd.tuigreet
