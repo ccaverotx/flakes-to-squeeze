54858890128000ec408aa7006fa718899dca4350
ccaverotx <ccaverotx@gmail.com>
2025-06-16 21:57:28 -0400

    refactor(hosts): added trace

--- modules/home/hosts/desktop/default.nix
+++ modules/home/hosts/desktop/default.nix
@@ -1,32 +1,34 @@
 { pkgs, lib, hostType, ... }:
 
-assert hostType == "desktop";  # Verificación estricta
+builtins.trace ">>> Host recibido en home/hosts/desktop: ${hostType}" (
+  assert hostType == "desktop";  # Verificación estricta
 
-{
-  home.username = "ccaverotx";
-  home.homeDirectory = lib.mkForce "/home/ccaverotx";
-  home.stateVersion = "24.05";
+  {
+    home.username = "ccaverotx";
+    home.homeDirectory = lib.mkForce "/home/ccaverotx";
+    home.stateVersion = "24.05";
 
-  imports = [
-    ../../programs/gui-apps/firefox
-    ../../programs/gui-apps/dbeaver
-    ../../programs/gui-apps/vscode
-    ../../programs/gui-apps/kitty
-    ../../programs/gui-apps/nemo
-    ../../programs/tui-apps/yazi
-    ../../programs/tui-apps/qutebrowser
-    ../../desktop/wms/hyprland
-    ../../desktop/wms/hyprland/xdg.nix
-  ];
+    imports = [
+      ../../programs/gui-apps/firefox
+      ../../programs/gui-apps/dbeaver
+      ../../programs/gui-apps/vscode
+      ../../programs/gui-apps/kitty
+      ../../programs/gui-apps/nemo
+      ../../programs/tui-apps/yazi
+      ../../programs/tui-apps/qutebrowser
+      ../../desktop/wms/hyprland
+      ../../desktop/wms/hyprland/xdg.nix
+    ];
 
-  home.packages = with pkgs; [
-    unzip
-    udiskie
-    nerd-fonts.fira-code
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.hack
-    nerd-fonts.noto
-    nerd-fonts.dejavu-sans-mono
-    font-awesome
-  ];
-}
+    home.packages = with pkgs; [
+      unzip
+      udiskie
+      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.hack
+      nerd-fonts.noto
+      nerd-fonts.dejavu-sans-mono
+      font-awesome
+    ];
+  }
+)
