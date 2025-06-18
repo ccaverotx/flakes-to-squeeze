c3a96e8b5b9ac3c8df58c2a252d2bdaa9ec2d64d
Carlos Cavero <54409608+ccaverotx@users.noreply.github.com>
2025-06-18 05:27:06 +0000

    Update lanzaboote.nix

--- modules/security/lanzaboote.nix
+++ modules/security/lanzaboote.nix
@@ -9,9 +9,8 @@
     };
   };
 
-  config = lib.mkIf config.secureboot.enable {
+  config = lib.mkIf (config.secureboot.enable && lib.hasAttrByPath [ "boot" "lanzaboote" ] config) {
     boot.loader.systemd-boot.enable = lib.mkForce false;
-
     boot.loader.efi.canTouchEfiVariables = true;
 
     boot.lanzaboote = {
